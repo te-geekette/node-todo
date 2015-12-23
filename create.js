9c6c8f78156f4a2a981887c1f74ce56563f6aec6
Tina Egolf <tinaegolf@gmail.com>
2015-12-23 18:14:05 +0100

    First approach to creating and viewing tasks

--- create.js
+++ create.js
@@ -1,12 +1,40 @@
-var list = require('./list'); 
-var oldList = list();
+var Pathwise = require('level-pathwise'); 
+var level = require('level');
 
-var task = process.argv.slice(2); 
+var todos = level('todos');
+var store = new Pathwise(todos); 
+
+var guid = require('node-uuid').v4;
+
+// Create a task 
+var input = process.argv.slice(2); // 1. Input: Task, 2. Input (optional): Due Date, 3. Input (optional): Status 
+var id = guid();
+
+store.put([], {
+	[id]: {
+		task: input[0],
+		dueDate: input[1] ? input[1] : 'At some point',
+		createDate: new Date(), 
+		status: input[2] ? input[2] : 'open' 
+	}
+}, function(err){
+	if(err){
+		console.error(err.message);
+	}
+});
 
 
-function addTaskToList (task) {
-	oldList.all.push(task[0]); 
-	oldList.today.push(task[0]);
-}
+// function addTaskToList (task) {
+// 	var taskObject = {
+// 		id: guid(),
+// 		task: input[0],
+// 		dueDate: input[1],
+// 		createDate: new Date(), 
+// 		status: input[2] ? input[2] : 'open' 
+// 	};
+// 	db.put(taskObject.id, taskObject);
+// }
 
-addTaskToList(task);
+// addTaskToList(input);
+
+
--- view.js
+++ view.js
@@ -1,10 +1,56 @@
-var list = require('./list'); 
-var showList = list();
+var Pathwise = require('level-pathwise'); 
+var level = require('level');
+
+var todos = level('todos');
+var store = new Pathwise(todos); 
+
+// View tasks depending on selection
+
 var selector = process.argv.slice(2); // Selects the correct list regarding time or status: all, today, open, done etc. 
 
-function displayList(selector) {
-	console.log(showList.selector[0]); 
-}
+store.get([], function(err, objectList){
+	for(var object in objectList){
+		store.get([object], function(error, objectItem){
+			console.log(objectItem.task + ': ' + objectItem.status); 
+		})
+	}
+	
+});
 
-displayList(selector); 
+// ToDo: 
+// 1. Find out about Filter (NewBloomFilterPolicy ?)
+// 2. Better way of displaying the tasks 
+
+// Questions:
+// 1. Are modules the best way to control the app or are there some console shortcuts? 
+// 
+
+
+
+
+
+
+
+// OLD TEST - DIDN'T WORK PROPERLY
+
+// function logList(element){
+// 	console.log(element);
+// }
+
+// function displayList(selector) {
+// 	var taskList = [];
+// 	var list = db.createValueStream(); 
+
+// 	list.on('data', function(data){
+// 		taskList.push(data); 
+// 	}); 
+
+// 	list.on('end', function(){
+// 		taskList.forEach(logList); 
+// 	}); 
+// }
+
+// displayList(selector); 
+
+
 
