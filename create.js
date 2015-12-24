5c9ea5ae3f1b1bd7f7b28b6af67afe150849a8bd
Tina Egolf <tinaegolf@gmail.com>
2015-12-24 15:02:12 +0100

    Custom id and some experiements around get

--- create.js
+++ create.js
@@ -2,14 +2,33 @@ var Pathwise = require('level-pathwise');
 var level = require('level');
 
 var todos = level('todos');
+var todoCount = level('count');
 var store = new Pathwise(todos); 
 
-var guid = require('node-uuid').v4;
+// Custom input to specify details for the task
+var input = process.argv.slice(2); // 1. Input: Task, 2. Input (optional): Due Date, 3. Input (optional): Status 
+
+
+// Create a custom id for the task (to make it easier to retrieve and update later)
+var id; 
+var currentCount = todoCount.createValueStream();
+var countList = []; 
+
+currentCount.on('data', function(data){
+	countList.push(data);
+});
+currentCount.on('end', function(){
+	if (countList.length === 0){
+		todoCount.put('Count', '1');
+		id = 1; 
+
+	} else {
+		id = parseInt(countList[0]) + 1 ;
+		todoCount.put('Count', id ); 
+	}
+}); 
 
 // Create a task 
-var input = process.argv.slice(2); // 1. Input: Task, 2. Input (optional): Due Date, 3. Input (optional): Status 
-var id = guid();
-
 store.put([], {
 	[id]: {
 		task: input[0],
@@ -22,19 +41,5 @@ store.put([], {
 		console.error(err.message);
 	}
 });
-
-
-// function addTaskToList (task) {
-// 	var taskObject = {
-// 		id: guid(),
-// 		task: input[0],
-// 		dueDate: input[1],
-// 		createDate: new Date(), 
-// 		status: input[2] ? input[2] : 'open' 
-// 	};
-// 	db.put(taskObject.id, taskObject);
-// }
-
-// addTaskToList(input);
 
 
--- view.js
+++ view.js
@@ -1,3 +1,4 @@
+// Prerequisits
 var Pathwise = require('level-pathwise'); 
 var level = require('level');
 
@@ -5,25 +6,45 @@ var todos = level('todos');
 var store = new Pathwise(todos); 
 
 // View tasks depending on selection
-
 var selector = process.argv.slice(2); // Selects the correct list regarding time or status: all, today, open, done etc. 
 
+// Fetches all task-objects from the store as one big object
 store.get([], function(err, objectList){
-	for(var object in objectList){
-		store.get([object], function(error, objectItem){
-			console.log(objectItem.task + ': ' + objectItem.status); 
-		})
-	}
+	var finalList = [];
+
+	//This is supposed to do: the incoming data from objectList ... 
+	objectList.on('data', function(objects){
+		console.log(objects);
+
+		// should extract each object in the list and ...
+		for(var object in objects){
+			// get it's properties
+			store.get([object], function(error, objectItem){
+				// if the status property is specified in the console input ..
+				if (objectItem.status === selector[0]) {
+					// add the task stored in this object to the finalList array
+					finalList.push(objectItem.task);
+				}
+			});
+		} 
+	});
+
+	// once the data flow has finished ...
+	objectList.on('end', function(){
+		// log out the finalList with all tasks that match the console input
+		console.log(finalList);
+	});
 	
 });
 
+
 // ToDo: 
-// 1. Find out about Filter (NewBloomFilterPolicy ?)
-// 2. Better way of displaying the tasks 
+// Better way of displaying the tasks 
 
 // Questions:
-// 1. Are modules the best way to control the app or are there some console shortcuts? 
-// 
+// Why is the get - on logic not working properly?  
+// Are modules the best way to control the app or are there some console shortcuts? 
+//
 
 
 
@@ -32,6 +53,20 @@ store.get([], function(err, objectList){
 
 
 // OLD TEST - DIDN'T WORK PROPERLY
+
+// store.get([], function(err, objectList){
+// 	for(var object in objectList){
+// 			store.get([object], function(error, objectItem){
+// 				console.log(selector[0], objectItem.status);
+// 				if (objectItem.status === selector[0]) {
+// 					finalList.push(objectItem.task);
+// 				}
+// 				// console.log(objectItem.task + ': ' + objectItem.status); 
+// 			});
+// 	} 
+// });
+
+// ---------
 
 // function logList(element){
 // 	console.log(element);
@@ -51,6 +86,30 @@ store.get([], function(err, objectList){
 // }
 
 // displayList(selector); 
+
+// -----------
+
+// EXPERIMENT
+
+// function logList(element){
+// 	console.log(element);
+// }
+
+// function displayList(selector) {
+// 	var taskList = [];
+// 	var testList = todos.createValueStream();
+
+// 	testList.on('data', function(data){
+// 		taskList.push(data); 
+// 	}); 
+
+// 	testList.on('end', function(){
+// 		console.log(taskList);
+// 		//taskList.forEach(logList); 
+// 	}); 
+// }
+
+//displayList(selector); 
 
 
 
